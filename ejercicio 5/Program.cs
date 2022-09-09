dd74a26191d1c4f89d926700535624d0584168b1
CARLOS GUERRERO <CARLOS GUERRERO@LAPTOP-RS0CRK9G>
2022-09-08 19:28:31 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio 5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ejercicio5
+{
+    public class program
+    {
+        public static void Main()
+        {
+
+            int limite;
+            double cuadrado;
+            int numero = 2;
+            int elevado;
+            elevado = 0;
+            limite = 10;
+            for (elevado = 0; elevado <= limite; elevado++)
+            {
+                cuadrado = Math.Pow(numero, elevado);
+                Console.WriteLine($"El resultado del número 2 al cuadrado de {elevado} es : {cuadrado}");
+            }
+
+        }
+    }
+}
